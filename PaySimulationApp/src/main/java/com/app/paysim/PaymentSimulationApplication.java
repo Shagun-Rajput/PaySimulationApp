e9c157b348323720dc3cbcb8de8620c4677108f5
Shagun Rajput <shagun.rajput@prod.hclpnp.com>
2025-08-30 17:01:19 +0530

    application setup

--- PaySimulationApp/src/main/java/com/app/paysim/PaymentSimulationApplication.java
+++ PaySimulationApp/src/main/java/com/app/paysim/PaymentSimulationApplication.java
@@ -1,14 +1,14 @@
-package com.app.dvm;
+package com.app.paysim;
 
 import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DVMApplication {
-    private static final Logger logger = org.slf4j.LoggerFactory.getLogger(DVMApplication.class);
+public class PaymentSimulationApplication {
+    private static final Logger logger = org.slf4j.LoggerFactory.getLogger(PaymentSimulationApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(DVMApplication.class, args);
+        SpringApplication.run(PaymentSimulationApplication.class, args);
         logger.info("********************************************************************");
         logger.info("****** Payment Simulation Application started successfully *********");
         logger.info("********************************************************************");
