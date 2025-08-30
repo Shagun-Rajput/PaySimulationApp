843cfa74daed29afde1d441efe15af9c0d2dab8b
Shagun Rajput <shagun.rajput@prod.hclpnp.com>
2025-08-30 19:34:11 +0530

    changed uri

--- PaySimulationApp/src/main/java/com/app/paysim/constant/ApiURIs.java
+++ PaySimulationApp/src/main/java/com/app/paysim/constant/ApiURIs.java
@@ -3,5 +3,5 @@ package com.app.paysim.constant;
 public class ApiURIs {
     public static final String URI_INIT_PAYMENT = "/api/payment/initiate";
     public static final String URI_FIND_PAYMENTS = "/api/payments";
-    public static final String URI_USER_AUTH_GEN = "/api/user-auth-token";
+    public static final String URI_USER_AUTH_GEN = "/auth/generate-token";
 }
